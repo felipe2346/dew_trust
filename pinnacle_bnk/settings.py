a54a1335a13285ade45fd705b17637865fee867e
NephygTdkingz <tdkingzict@gmail.com>
2025-02-08 18:07:20 +0100

    added vercel to host and removed my sql from init

--- pinnacle_bnk/settings.py
+++ pinnacle_bnk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com','pinnacleonlinetb.com','www.pinnacleonlinetb.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost']
 
 
 # Application definition
