fc37c02766f077d22c9e3cf03ce4c0f979287872
NephygTdkingz <tdkingzict@gmail.com>
2025-02-10 14:00:43 +0100

    added domain to host

--- pinnacle_bnk/settings.py
+++ pinnacle_bnk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost']
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost', 'dewtrustonlinefb.com', 'www.dewtrustonlinefb.com']
 
 
 # Application definition
